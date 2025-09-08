dd9e40ea7143eab6540b16c54800a4683f9dc693
miklymchuk <miklymchuk@gmail.com>
2025-09-08 09:29:34 -0400

    Added file.

---
+++ JavaFXLab.java
@@ -0,0 +1,23 @@
+package javafxlab;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ *
+ * @author 6312398
+ */
+public class JavaFXLab extends Application {
+
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start (Stage primaryStage) {
+        primaryStage.setTitle("Graded Lab");
+        
+        primaryStage.show();
+    }
+    
+}
